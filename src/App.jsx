a61e34b5333fd88f942e5441cd9e5cb847a1b4af
anirudhg001 <annigupta007@gmail.com>
2024-02-10 21:41:56 +0530

    updated: name of the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   const [count, setCount] = useState(0);
 
-  return <div>3D Developer Portfolio</div>;
+  return <div>3D Developer Portfolio- with responsiveness and animations</div>;
 };
 
 export default App;
