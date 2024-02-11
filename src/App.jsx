62f7e0ed48cb27409c85aee7b8563a4968b1350e
anirudhg001 <annigupta007@gmail.com>
2024-02-11 20:17:27 +0530

    fixed: folder structure and jsx imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => {
-  const [count, setCount] = useState(0);
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
-  return <div>3D Developer Portfolio- with responsiveness and animations</div>;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"></div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
